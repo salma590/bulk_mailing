27e7cf5b2fe6685353cbff635ac030b37917652e
salma590 <120380618+salma590@users.noreply.github.com>
2024-02-14 23:38:57 +0530

    Update setup.sh

--- biomedres/setup.sh
+++ biomedres/setup.sh
@@ -1,2 +1,14 @@
+#!/bin/sh
 cd /home/ubuntu
-mkdir salma2
+mkdir biomed
+cd biomed
+sudo apt-get update
+sudo apt install python
+sudo apt update
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+sudo python2 get-pip.py
+pip install requests
+pip install sendgrid
+pip2 install sendgrid
+
+screen -dmS biomed pyhton test.py
